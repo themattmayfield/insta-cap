36767aa06e35dbd8198568971004afd63e0dc43d
Matthew Mayfield <mattmayf411@gmail.com>
2024-03-03 15:58:05 -0600

    hmm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,26 +10,27 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from '@/components/ui/drawer';
-// import { sql } from '@vercel/postgres';
-// import pluralize from 'pluralize';
+import { sql } from '@vercel/postgres';
+import pluralize from 'pluralize';
 
 export default async function Home() {
-  // const [tokenResponse, { rows }] = await Promise.all([
-  //   fetch(`${process.env.WEB_URL}/api/token`),
-  //   sql`SELECT COUNT(*) FROM Prompts;`,
-  // ]);
-  // const count = rows[0].count;
-  // const numberOfCaptions = pluralize('captions', count, true);
+  const [tokenResponse, { rows }] = await Promise.all([
+    fetch(`${process.env.WEB_URL}/api/token`),
+    sql`SELECT COUNT(*) FROM Prompts;`,
+  ]);
+  const count = rows[0].count;
+  const numberOfCaptions = pluralize('captions', count, true);
 
-  // const { token } = await tokenResponse.json();
+  const { token } = await tokenResponse.json();
   return (
     <div className="py-[15vh] sm:py-[20vh] flex flex-col items-center justify-center">
       <h1 className="font-medium text-4xl text-black text-center max-w-[700px] mb-3 animate-in fade-in slide-in-from-bottom-3 duration-1000 ease-in-out">
         Insta captions
       </h1>
-      {/* <p className="text-gray-500 text-center mb-12 text-base animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
+      <p className="text-gray-500 text-center mb-12 text-base animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
         {numberOfCaptions} generated and counting!
-      </p> */}
+      </p>
+      {token}
 
       <div className="max-w-md space-y-4 w-full animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
         <div>
